2b55e5ec10c58133eb1d2f7726ba6d904cfc762c
Gkiouliavas <kgkiouliavas@deloitte.gr>
2022-12-13 22:56:07 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getGreatestCommonDivisor(25,15));
+        System.out.println(getGreatestCommonDivisor(12,30));
+        System.out.println(getGreatestCommonDivisor(9,18));
+        System.out.println(getGreatestCommonDivisor(81,153));
+    }
+
+    public static int getGreatestCommonDivisor(int first , int second){
+        if(first<10 || second<10){
+            return -1;
+        } else {
+            int i = Math.max(first, second);
+            while (true){
+                if((first%i==0) && (second%i==0)){
+                    return i;
+                }
+            i--;
+            }
+
+        }
+
+        }
+
+}
